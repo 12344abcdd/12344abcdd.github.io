cca443d934c23e44d678feb8a382487e27772ffe
abcdd123441 <abcdd_12344_1@outlook.com>
2025-08-27 17:07:25 +0800

    Add files via upload

---
+++ manager_n.js
@@ -0,0 +1,6 @@
+import { initUI, showActions, showPath, updateHeaderInfo, showStatus } from './js/ui-handlers.js';
+
+// 初始化 UI
+document.addEventListener('DOMContentLoaded', () => {
+    initUI();
+});
