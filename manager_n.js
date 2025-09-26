da081a7ad6d07f687027743966264513642f4412
12344abcdd <abcdd_12344@outlook.com>
2025-09-26 12:14:16 +0800

    1

--- manager_n.js
+++
@@ -1,6 +0,0 @@
-import { initUI, showActions, showPath, updateHeaderInfo, showStatus } from './manager/ui-handlers.js';
-
-// 初始化 UI
-document.addEventListener('DOMContentLoaded', () => {
-    initUI();
-});
